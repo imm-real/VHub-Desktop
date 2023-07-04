d0ff885a83ed2f76681404d258692798b8b2d46c
MichaelScripter <97390242+MichaelScripter@users.noreply.github.com>
2023-07-04 10:20:21 -0700

    Update slider.lua

--- classes/slider.lua
+++ classes/slider.lua
@@ -2,9 +2,21 @@ local slider = {}
 slider.__index = slider
 
 local RunService = game:GetService("RunService")
+local UserInputService = game:GetService('UserInputService')
 
 local Player = game:GetService("Players").LocalPlayer
 local Mouse = Player:GetMouse()
+
+function slider:IsInBounds()
+	local Frame = self.Frame
+	
+	local X, Y = Mouse.X - Frame.AbsolutePosition.X, Mouse.Y - Frame.AbsolutePosition.Y
+	local MaxX, MaxY = Frame.AbsoluteSize.X, Frame.AbsoluteSize.Y
+	
+	if (X >= 0 and X <= MaxX) and (Y >= 0 and Y <= MaxY) then
+		return X/MaxX, Y/MaxY
+	end
+end
 
 function slider:GetOffset()
 	local Frame = self.Frame
@@ -17,7 +29,7 @@ end
 
 function slider:Fire(Value)
 	if not self._func then return end
-
+	
 	self._func(math.floor(self.Min+(self.Diff*Value)))
 end
 
@@ -27,7 +39,7 @@ end
 
 function slider:UpdateGradient(Value)
 	local Frame = self.Frame
-
+	
 	if Value <= 0.01 then
 		Frame.Bar.UIGradient.Transparency = NumberSequence.new(1, 1)
 		return
@@ -46,20 +58,20 @@ end
 
 function slider:Set(value)
 	local Frame = self.Frame
-
+	
 	Frame.Point.Position = UDim2.fromScale(value, 0.5)
 	self:UpdateGradient(value)
 end
 
 function slider:UpdateSlider()
 	if not self.MouseDown then return end
-
+	
 	local Value = self:GetOffset()
 	self:Set(Value)
 	self:Fire(Value)
 end
 
-function slider:Init(SliderFrame, Max, Min)
+function slider:New(SliderFrame, Max, Min)
 	self = {
 		MouseDown = false,
 		Frame = SliderFrame.Bar,
@@ -71,13 +83,16 @@ function slider:Init(SliderFrame, Max, Min)
 	Mouse.Move:Connect(function()
 		self:UpdateSlider()
 	end)
-	self.Frame.InputBegan:Connect(function(Input)
-		if Input.UserInputType == Enum.UserInputType.MouseButton1 then return end
+	UserInputService.InputBegan:Connect(function(Input)
+		if not (Input.UserInputType == Enum.UserInputType.MouseButton1) then return end
+		if not self:IsInBounds() then return end
+		
 		self.MouseDown = true
-		Input.Changed:Connect(function()
-			if Input.UserInputState == Enum.UserInputState.End then return end
-			self.MouseDown = false
-		end)
+	end)
+	UserInputService.InputEnded:Connect(function(Input)
+		if not (Input.UserInputType == Enum.UserInputType.MouseButton1) then return end
+
+		self.MouseDown = false
 	end)
 	return setmetatable(self, slider)
 end
