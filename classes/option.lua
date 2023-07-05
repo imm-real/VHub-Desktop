31eced60285098150009fc91893d6224f4f08fc9
MichaelScripter <97390242+MichaelScripter@users.noreply.github.com>
2023-07-05 05:09:15 -0700

    Create option.lua

---
+++ classes/option.lua
@@ -0,0 +1,42 @@
+local option = {}
+
+local Services = {
+	HttpService = game:GetService('HttpService')
+}
+
+function option:New(Name, CheckFunction)
+	if not _G.Vhub.Options[Name] then return end
+	self = {
+		option = Name,
+		func = CheckFunction
+	}
+	task.spawn(function() self:Load() end)
+	return setmetatable(self, option)
+end
+
+function option:Save(Value)
+	local Options = {}
+	if isfile('vhub_options.json') then
+		Options = Services.HttpService:JSONDecode(readfile('vhub_options.json'))
+	end
+	
+	Options[self.option] = Value
+	writefile('vhub_options.json', Services.HttpService:JSONEncode(Options))
+end
+
+function option:Load()
+	if not isfile('vhub_options.json') then return end
+	local Options = Services.HttpService:JSONDecode(readfile('vhub_options.json'))
+	
+	if not Options[self.option] then return end
+	if not self.func(Options[self.option]) then return end
+	_G.Vhub.Options[self.option] = Options[self.option]
+end
+
+function option:Set(Value)
+	if not self.func(Value) then return end
+	
+	_G.Vhub.Options[self.option] = Value
+end
+
+return option
