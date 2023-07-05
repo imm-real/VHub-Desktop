18ed0d4362823036ad8d5fd79904e27d3624cdd4
MichaelScripter <97390242+MichaelScripter@users.noreply.github.com>
2023-07-05 05:49:51 -0700

    Update option.lua

--- classes/option.lua
+++ classes/option.lua
@@ -10,31 +10,30 @@ function option:Save(Value)
 	if isfile('options.vh') then
 		Options = Services.HttpService:JSONDecode(readfile('options.vh'))
 	end
-	print(self.option)
-	Options[self.option] = Value
+	Options[self.name] = Value
 	writefile('options.vh', Services.HttpService:JSONEncode(Options))
 end
 
 function option:Load()
 	if not isfile('options.vh') then return end
 	local Options = Services.HttpService:JSONDecode(readfile('options.vh'))
-	
-	if not Options[self.option] then return end
-	if not self.func(Options[self.option]) then return end
-	_G.Vhub.Options[self.option] = Options[self.option]
+
+	if not Options[self.name] then return end
+	if not self.func(Options[self.name]) then return end
+	_G.Vhub.Options[self.name] = Options[self.name]
 end
 
 function option:Set(Value)
 	if not self.func(Value) then return end
-	
-	_G.Vhub.Options[self.option] = Value
+
+	_G.Vhub.Options[self.name] = Value
 	option:Save(Value)
 end
 
 function option:New(Name, CheckFunction)
 	if not _G.Vhub.Options[Name] then return end
 	self = {
-		option = Name,
+		name = Name,
 		func = CheckFunction
 	}
 	return setmetatable(self, option)
