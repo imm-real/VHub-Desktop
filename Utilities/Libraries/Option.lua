e4561b5deb9b7c7a145b08ea17e0464dc784be7c
MichaelScripter <97390242+MichaelScripter@users.noreply.github.com>
2023-08-23 03:46:44 -0700

    Update Option.lua

--- Utilities/Libraries/Option.lua
+++ Utilities/Libraries/Option.lua
@@ -1,52 +1,58 @@
 local Option = {}
 Option.__index = Option
 
-local Services = {
-	HttpService = game:GetService('HttpService')
-}
+HttpService = game:GetService('HttpService')
 
-local Checks = {}
+local SavedOptions = {}
 
-function Option:Load()
+function Option.load()
 	if not isfile('options.vh') then return end
-	local Options = Services.HttpService:JSONDecode(readfile('options.vh'))
+	local succes, error = pcall(function() 
+		local Options = HttpService:JSONDecode(readfile('options.vh'))
+		for Option, Value in pairs(Options) do
+			if not SavedOptions[Option] then continue end
+			if not SavedOptions[Option](Value) then continue end
 
-	for Option, Value in pairs(Options) do
-		if not Checks[Option] then continue end
-		if not Checks[Option](Value) then continue end
-		
-		if Option:sub(0,4) == 'JSON' then
-			_G.Vhub.Options[Options] = Services.HttpService:JSONDecode(Value)
-		else
-			_G.Vhub.Options[Option] = Value
+			if Option:sub(0,4) == 'JSON' then
+				_G.Vhub.Options[Options] = HttpService:JSONDecode(Value)
+			else
+				_G.Vhub.Options[Option] = Value
+			end
+		end 
+	end)
+	if not succes then
+		warn('[Vhub Error]: '..error)
+	end
+end
+
+function Option.save(Value)
+	local succes, error = pcall(function()
+		if not self.check_function(Value) then return end
+
+		local Options = {}
+		if isfile('options.vh') then
+			Options = HttpService:JSONDecode(readfile('options.vh'))
 		end
+		if self.name:sub(0,4) == 'JSON' then
+			Options[self.name] = HttpService:JSONEncode(Value)
+		else
+			Options[self.name] = Value
+		end
+		writefile('options.vh', HttpService:JSONEncode(Options))
+	end)
+	if not succes then
+		warn('[Vhub Error]: '..error)
 	end
 end
 
-function Option:Save(Value)
-	if not self.func(Value) then return end
-
-	local Options = {}
-	if isfile('options.vh') then
-		Options = Services.HttpService:JSONDecode(readfile('options.vh'))
-	end
-	if self.name:sub(0,4) == 'JSON' then
-		Options[self.name] = Services.HttpService:JSONEncode(Value)
-	else
-		Options[self.name] = Value
-	end
-	writefile('options.vh', Services.HttpService:JSONEncode(Options))
-end
-
-function Option:New(Name, CheckFunction)
+function Option.new(Name, CheckFunction)
 	if not _G.Vhub.Options[Name] then return end
-	self = {
+	local self = {
 		name = Name,
-		func = CheckFunction
+		check_function = CheckFunction
 	}
-	Checks[Name] = CheckFunction
-	setmetatable(self, Option)
-	return self
+	SavedOptions[Name] = CheckFunction
+	return setmetatable(self, Option)
 end
 
 return Option
